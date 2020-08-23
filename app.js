3862b54eafe3c121cfa2ef67c0e7ee3d12c4a34d
Shane <shaneroland1@gmail.com>
2020-08-22 20:37:10 -0400

    Version 1.4: board movement and socket
    -Revamped index page
    -Implemented sockets
    -Implemented board and piece movement
    -Removed external style sheet and made it internal to index.html file
    -Added socket.io and client as dependencies
    -Deleted pictures
    -Deleted image folder

--- app.js
+++ app.js
@@ -1,19 +1,105 @@
 var express = require('express');
 var path = require('path');
+const { request } = require('http');
 
 //init express
 var app = express();
 
+const http = require('http').Server(app);
+
+const socket = require('socket.io')(http);
 
 //Variables 
 var PORT = process.env.PORT || 5000;
 
 //Create endpoint handlers
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 //listen on port
-app.listen(PORT, function() {console.log(`server started on ${PORT}`);}
-);
+//app.listen(PORT, function() {console.log(`server started on ${PORT}`);}
+//);
+
+app.get('/', (req, res) => res.sendFile(__dirname+'/index.html'));
+
+const PIECES = {
+    black: {
+        "king": "&#x265A;",
+        "queen": "&#x265B;",
+        "rook": "&#x265C;",
+        "bishop": "&#x265D;",
+        "knight": "&#x265E;",
+        "pawn": "&#x265F;"
+    },
+    white: {
+        "king": "&#x2654;",
+        "queen": "&#x2655;",
+        "rook": "&#x2656;",
+        "bishop": "&#x2657;",
+        "knight": "&#x2658;",
+        "pawn": "&#x2659;"
+    }
+}
+
+
+var b = PIECES.black, w = PIECES.white;
+
+var board = {
+    a1: b.rook,
+    a2: b.knight,
+    a3: b.bishop,
+    a4: b.queen,
+    a5: b.king,
+    a6: b.bishop,
+    a7: b.knight,
+    a8: b.rook,
+    b1: b.pawn,
+    b2: b.pawn,
+    b3: b.pawn,
+    b4: b.pawn,
+    b5: b.pawn,
+    b6: b.pawn,
+    b7: b.pawn,
+    b8: b.pawn,
+    g1: w.pawn,
+    g2: w.pawn,
+    g3: w.pawn,
+    g4: w.pawn,
+    g5: w.pawn,
+    g6: w.pawn,
+    g7: w.pawn,
+    g8: w.pawn,
+    h1: w.rook,
+    h2: w.knight,
+    h3: w.bishop,
+    h4: w.queen,
+    h5: w.king,
+    h6: w.bishop,
+    h7: w.knight,
+    h8: w.rook
+}
+socket.on('test', data => {
+    console.log(`message recieved: ${data.test}`)
+});
+
+socket.on('connection', socket => {
+    //console.log('user connected');
+    
+    socket.on('move-piece', data => {
+        //console.log('move event recieved');
+        board[data.end] = board[data.start];
+        board[data.start] = "";
+        socket.emit('update', {
+            board: board
+        });
+    });
+    socket.emit('update', {
+        board: board
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+});
 
 class ChessGame{
     constructor(){
