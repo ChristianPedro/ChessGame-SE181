5b59fe1f01e6c04c340e3c3cdee583890dc0f810
sp3573 <sp3573@drexel.edu>
2021-02-07 21:55:53 -0500

    Don't panic

--- getPosition.test.js
+++
@@ -1,8 +0,0 @@
-const getPosition = require('./app');
-
-test('This function checks of input is split into x and y', () => {
-    function noXY() {
-        getPosition(0);
-    }
-    expect(noXY).toThrowError('invalid input');
-});
--- isWhitePiece.test.js
+++
@@ -1,9 +0,0 @@
-const isWhitePiece = require('./app');
-
-test('This function checks that the function getRoom throws an error when a negative index is fed', () => {
-    function notWhite() {
-        isWhitePiece(-1);
-    }
-    expect(notWhite).toThrowError('Black piece');
-});
-
